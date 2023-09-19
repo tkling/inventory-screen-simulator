252168e40c13127d84832c72046341878b6f44aa
tkling <tyling@gmail.com>
2023-09-18 23:03:35 -0400

    drag and drop

--- app/main.rb
+++ app/main.rb
@@ -20,12 +20,14 @@ class InventoryScreenSimulator
 
   def defaults
     state.welcomed_at = state.tick_count
+    state.currently_dragging_item_id = nil
 
     state.padding = 16
     inv_grid_columns = 10
     state.inv_grid_item_scale = 1.5
     state.r_panel_width = inv_grid_columns * 32 * state.inv_grid_item_scale
     state.panel_label_h = 15
+    state.all_grid_cells = []
 
     state.inventory_grid = {
       padding: state.padding,
@@ -44,8 +46,9 @@ class InventoryScreenSimulator
     state.character_panel = {}
 
     shikashi_sprite = {w: 48, h: 48, tile_w: 32, tile_h: 32, path: "sprites/shikashi/transparent_drop_shadow.png"}
-    state.items = [
-      {
+    state.items = {
+      0 => {
+        id: 0,
         name: "potion",
         desc: "Heals you lol.",
         consumable: true,
@@ -55,7 +58,8 @@ class InventoryScreenSimulator
         tile_x: 12 * 32,
         tile_y: 9 * 32
       },
-      {
+      1 => {
+        id: 1,
         name: "short sword",
         desc: "A sword, but not very big.",
         consumable: false,
@@ -65,7 +69,8 @@ class InventoryScreenSimulator
         tile_x: 2 * 32,
         tile_y: 5 * 32
       },
-      {
+      2 => {
+        id: 2,
         name: "strawberry",
         desc: "A tasty treat that restores a bit of health.",
         consumable: true,
@@ -75,7 +80,7 @@ class InventoryScreenSimulator
         tile_x: 4 * 32,
         tile_y: 14 * 32
       }
-    ]
+    }
 
     state.character = {
       name: "Charmander Smith",
@@ -130,7 +135,6 @@ class InventoryScreenSimulator
     inv_grid = state.inventory_grid
     row_count, column_count = inv_grid.values_at(:cells_y, :cells_x)
     row_height = inv_grid[:h] / row_count
-    column_width = inv_grid[:w] / column_count
     left_side_x = inv_grid.values_at(:w, :padding).sum.from_right
 
     outputs.labels << {
@@ -140,32 +144,32 @@ class InventoryScreenSimulator
       alignment_enum: 1
     }
 
-    outputs.borders << 0.upto(row_count - 1).map do |i|
-      {
-        x: left_side_x,
-        y: inv_grid[:padding] + row_height * i,
-        w: inv_grid[:w],
-        h: row_height
-      }
-    end
+    outputs.borders << 0.upto(row_count - 1).map do |ri|
+      y = inv_grid[:padding] + row_height * ri
+      0.upto(column_count - 1).map do |ci|
+        {
+          x: left_side_x + 48 * ci,
+          y: y,
+          w: row_height,
+          h: row_height,
+          grid_loc: [ci, ri],
+          grid: :inventory
+        }
+      end
+    end.flatten.tap { |cells| state.all_grid_cells += cells }
 
-    outputs.borders << 0.upto(column_count - 1).map do |i|
-      {
-        x: left_side_x + column_width * i,
-        y: inv_grid[:y],
-        w: column_width,
-        h: inv_grid[:h]
-      }
-    end
-
-    items = state.items.select { |item| item[:grid] == :inventory }
+    items = state.items.values.select { |item| item[:grid] == :inventory }
     outputs.sprites << items.map do |item|
       xd, yd = item[:grid_loc]
       offset_scale = 32 * state.inv_grid_item_scale
-      item.merge(
-        x: left_side_x + xd * offset_scale,
-        y: inv_grid[:y] + yd * offset_scale
-      )
+      item.tap do |i|
+        unless i.id == state.currently_dragging_item_id
+          i.merge!(
+            x: left_side_x + xd * offset_scale,
+            y: inv_grid[:y] + yd * offset_scale
+          )
+        end
+      end
     end
   end
 
@@ -245,18 +249,24 @@ class InventoryScreenSimulator
         x: hotbar_x + 48 * i,
         y: state.padding,
         w: 48,
-        h: 48
+        h: 48,
+        grid_loc: [i, 0],
+        grid: :hotbar
       }
-    end
+    end.tap { |cells| state.all_grid_cells += cells }
 
-    items = state.items.select { |i| i[:grid] == :hotbar }
+    items = state.items.values.select { |i| i[:grid] == :hotbar }
     outputs.sprites << items.map do |item|
       xd, _yd = item[:grid_loc]
       offset_scale = 32 * state.inv_grid_item_scale
-      item.merge(
-        x: hotbar_x + xd * offset_scale,
-        y: state.padding
-      )
+      item.tap do |i|
+        unless item.id == state.currently_dragging_item_id
+          item.merge!(
+            x: hotbar_x + xd * offset_scale,
+            y: state.padding
+          )
+        end
+      end
     end
   end
 
@@ -290,6 +300,35 @@ class InventoryScreenSimulator
   def handle_input
     gtk.request_quit if inputs.keyboard.key_down.escape
     defaults if inputs.keyboard.key_down.r || inputs.keyboard.key_down.enter
+
+    if state.currently_dragging_item_id
+      item = state.items[state.currently_dragging_item_id]
+      item_under_mouse = item
+    else
+      item = nil
+      item_under_mouse = nil
+    end
+
+    if inputs.mouse.click
+      if item_under_mouse ||= geometry.find_intersect_rect(inputs.mouse, state.items.values)
+        state.currently_dragging_item_id = item_under_mouse.id
+        state.mouse_point_inside_item = {
+          x: inputs.mouse.x - item_under_mouse.x,
+          y: inputs.mouse.y - item_under_mouse.y
+        }
+      end
+    elsif inputs.mouse.held && state.currently_dragging_item_id
+      item.x = inputs.mouse.x - state.mouse_point_inside_item.x
+      item.y = inputs.mouse.y - state.mouse_point_inside_item.y
+    elsif inputs.mouse.up
+      state.currently_dragging_item_id = nil
+      if (grid_cell_under_mouse = geometry.find_intersect_rect(inputs.mouse, state.all_grid_cells))
+        if grid_cell_under_mouse.grid != :hotbar || item.consumable
+          item.grid_loc = grid_cell_under_mouse.grid_loc
+          item.grid = grid_cell_under_mouse.grid
+        end
+      end
+    end
   end
 
   def calc
